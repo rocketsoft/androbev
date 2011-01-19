1a41e210161a4024d3aab0b7dd8b09bd51d956f6
mail4ajai@gmail.com <mail4ajai@gmail.com@f0f2cabb-5fe4-35e1-2ae5-732bf682352d>
2011-01-19 02:55:47 +0000

    check in initial copy of code added: Main UI

---
+++ trunk/src/com/ajsoft/androbev/AndroBev.java
@@ -0,0 +1,13 @@
+package com.ajsoft.androbev;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroBev extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
